5907a00a655e55e9739f29f277d1a0b28f23788b
george.grigorescu <george.grigorescu@netcentric.biz>
2020-01-29 19:46:25 +0200

    add bomboana for map runner

---
+++ src/main/java/Bomboana.java
@@ -0,0 +1,19 @@
+public class Bomboana {
+
+    private String name;
+
+    public Bomboana(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String toString() {
+        return "Bomboana{" +
+                "name='" + name + '\'' +
+                '}';
+    }
+}
